74150686de214f787f1f37ee4fd81700558ac62d
Alex Botelho <alexbotelho1@hotmail.com>
2020-06-05 18:31:52 -0300

    Mobile páginas ok

--- mobile/src/pages/Details/index.tsx
+++ mobile/src/pages/Details/index.tsx
@@ -1,15 +1,47 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import { View, Text, Image, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler'
 
 const Details = () => {
   const navigation = useNavigation();
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
   
   return (
     <>
-      <View>
-        <Text>Detials</Text>
-      </View>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <TouchableOpacity onPress={handleNavigateBack}>
+              <Icon name="arrow-left" size={20} color="#34cb79" />
+            </TouchableOpacity>
+
+          <Image style={styles.pointImage} source={{ uri: "https://images.unsplash.com/photo-1501523460185-2aa5d2a0f981?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }} />
+
+          <Text style={styles.pointName}>Mercado</Text>
+          <Text style={styles.pointItems}>Lâmpadas, Óleo de Cozinha</Text>
+
+          <View style={styles.address}>
+            <Text style={styles.addressTitle}>Endereço</Text>
+            <Text style={styles.addressContent}>Joinville, SC</Text>
+          </View>
+        </View>
+        <View style={styles.footer}>
+          <RectButton style={styles.button} onPress={() => {}}>
+            <FontAwesome name="whatsapp" size={20} color='#fff'/>
+            <Text style={styles.buttonText}>Whatsapp</Text>
+          </RectButton>
+
+          <RectButton style={styles.button} onPress={() => {}}>
+            <Icon name="mail" size={20} color='#fff'/>
+            <Text style={styles.buttonText}>E-mail</Text>
+          </RectButton>
+        </View>
+      </SafeAreaView>
     </>
   )
 };
@@ -18,7 +50,7 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       padding: 32,
-      paddingTop: 20,
+      paddingTop: 20 + Constants.statusBarHeight,
     },
   
     pointImage: {
--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import Constants from 'expo-constants'
+import Constants from 'expo-constants';
 import { Feather as Icon } from '@expo/vector-icons';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import { SvgUri } from 'react-native-svg';
 
 const Points = () => {
   const navigation = useNavigation();
 
   function handleNavigateBack() {
     navigation.goBack();
+  }
+
+  function handleNavigateToDetail() {
+    navigation.navigate('Details');
   }
 
   return (
@@ -23,14 +28,71 @@ const Points = () => {
         <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
 
         <View style={styles.mapContainer}>
-          <MapView style={styles.map} />
+          <MapView 
+            style={styles.map} 
+            initialRegion={{ 
+              latitude: -26.2810518,
+              longitude: -48.8396063,
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014,
+            }} 
+          >
+            <Marker
+              onPress={handleNavigateToDetail}
+              style={styles.mapMarker}
+              coordinate={{ 
+                latitude: -26.2810518,
+                longitude: -48.8396063,
+              }} 
+            >
+              <View style={styles.mapMarkerContainer}>
+                <Image 
+                  style={styles.mapMarkerImage} 
+                  source={{ uri: "https://images.unsplash.com/photo-1501523460185-2aa5d2a0f981?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }} 
+                />
+                <Text style={styles.mapMarkerTitle}>Mercado</Text>
+              </View>
+            </Marker>
+          </MapView>
         </View>
       </View>
 
       <View style={styles.itemsContainer}>
-        <TouchableOpacity style={styles.selectedItem}>
+        <ScrollView 
+          horizontal 
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 20 }}
+        >
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.101:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
 
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.101:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.101:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.101:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.101:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.101:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+        </ScrollView>
       </View>
     </>
   )
