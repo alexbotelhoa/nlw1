8da3a0d610306dbb29bbc91ca74ca299ae84ee54
Alex Botelho <alexbotelho1@hotmail.com>
2020-06-06 22:58:19 -0300

    Projeto Finalizado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import knex from './database/connection';
+import { celebrate, Joi } from 'celebrate'
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -18,7 +18,25 @@ routes.get('/', (req, res) => {
 routes.get('/items', itemsController.index);
 
 routes.get('/points', pointsController.index);
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+    '/points', 
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            uf: Joi.string().required().max(2),
+            city: Joi.string().required(),
+            items: Joi.string().required(),            
+        })
+    }, {
+        abortEarly: false
+    }),
+    pointsController.create
+);
 routes.get('/points/:id', pointsController.show);
 
 export default routes;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,14 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-app.use('/items', express.static(path.resolve(__dirname, 'assets')))
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use('/items', express.static(path.resolve(__dirname, 'assets')));
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333);
